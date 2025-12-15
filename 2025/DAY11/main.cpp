4da4d88db5871c4513028739eba80b9b81e24e8e
AbhinavPJ <abhipj159@gmail.com>
2025-12-15 17:12:57 +0530

    I give up, D9P2,D10P2,D12 not done

---
+++ 2025/DAY11/main.cpp
@@ -0,0 +1,346 @@
+/**
+ * author:VectorVirtuoso
+ * created: 19:46:01 on 12-12-2025
+ **/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <bitset>
+#include <iterator>
+#include <numeric>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <array>
+#include <functional>
+#include <type_traits>
+#include <exception>
+#include <cassert>
+#include <climits>
+#include <cfloat>
+#include <cctype>
+#include <chrono>
+#include <complex>
+#include <random>
+#include <valarray>
+#include <locale>
+#include <assert.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <limits.h>
+#include <float.h>
+using namespace std;
+#define int ll
+#define endl '\n' // comment for interactive
+#define fast_io              \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define pb push_back
+#define re resize
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define all1(x) (x).begin() + 1, (x).end()
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define loop1(i, n) for (int i = 1; i <= n; i++)
+#define print(x) cout << #x << ": " << x << endl \
+                      << flush
+typedef long long ll;
+typedef vector<int> vi;
+typedef array<int, 2> ii;
+typedef array<int, 3> ti;
+typedef vector<ii> vii;
+typedef vector<ti> vti;
+typedef vector<vi> vvi;
+typedef priority_queue<int> pq;
+template <class T>
+bool ckmin(T &a, T b)
+{
+    bool B = a > b;
+    a = min(a, b);
+    return B;
+}
+template <class T>
+bool ckmax(T &a, T b)
+{
+    bool B = a < b;
+    a = max(a, b);
+    return B;
+}
+const int inf = 1e17;
+const int mod = 1e9 + 7;
+int expo(int a, int b)
+{
+    if (a == 0 && b == 0)
+    {
+        return 1;
+    }
+    if (a == 0)
+    {
+        return 0;
+    }
+    if (b == 1)
+    {
+        return a;
+    }
+    if (b == 0)
+    {
+        return 1;
+    }
+    int x = expo(a, b / 2);
+    if (b % 2 == 0)
+    {
+        return (x) * (x) % mod;
+    }
+    else
+    {
+        return (a * (x) % mod) * (x) % mod;
+    }
+}
+int fac(int a)
+{
+    if (a == 0 || a == 1)
+    {
+        // factorial[a] = 1;
+        return 1;
+    }
+    return (fac(a - 1) * a) % mod;
+}
+int nCr(int n, int r)
+{
+    int mod1 = fac(n);
+    int mod2 = expo(fac(r), mod - 2);
+    int mod3 = expo(fac(n - r), mod - 2);
+    return (((mod1 * mod2) % mod) * mod3) % mod;
+}
+class SegmentTree
+{
+private:
+    vector<long long> tree, lazy;
+    vector<bool> lazySet;
+    int n;
+
+    void build(vector<int> &arr, int node, int start, int end)
+    {
+        if (start == end)
+        {
+            tree[node] = arr[start];
+        }
+        else
+        {
+            int mid = (start + end) / 2;
+            build(arr, 2 * node + 1, start, mid);
+            build(arr, 2 * node + 2, mid + 1, end);
+            tree[node] = min(tree[2 * node + 1], tree[2 * node + 2]);
+        }
+    }
+
+    void propagate(int node, int start, int end)
+    {
+        if (lazySet[node])
+        {
+            tree[node] = lazy[node];
+
+            if (start != end)
+            {
+                lazySet[2 * node + 1] = true;
+                lazySet[2 * node + 2] = true;
+                lazy[2 * node + 1] = lazy[node];
+                lazy[2 * node + 2] = lazy[node];
+            }
+
+            lazy[node] = 0;
+            lazySet[node] = false;
+        }
+    }
+
+    void updateRange(int node, int start, int end, int l, int r, int value)
+    {
+        propagate(node, start, end);
+
+        if (start > r || end < l)
+        {
+            return;
+        }
+
+        if (start >= l && end <= r)
+        {
+            lazySet[node] = true;
+            lazy[node] = value;
+            propagate(node, start, end);
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        updateRange(2 * node + 1, start, mid, l, r, value);
+        updateRange(2 * node + 2, mid + 1, end, l, r, value);
+
+        tree[node] = min(tree[2 * node + 1], tree[2 * node + 2]);
+    }
+
+    long long queryRange(int node, int start, int end, int l, int r)
+    {
+        propagate(node, start, end);
+
+        if (start > r || end < l)
+        {
+            return LLONG_MAX; // Neutral value for min query
+        }
+
+        if (start >= l && end <= r)
+        {
+            return tree[node];
+        }
+
+        int mid = (start + end) / 2;
+        long long leftQuery = queryRange(2 * node + 1, start, mid, l, r);
+        long long rightQuery = queryRange(2 * node + 2, mid + 1, end, l, r);
+
+        return min(leftQuery, rightQuery);
+    }
+
+public:
+    SegmentTree(vector<int> &arr)
+    {
+        n = arr.size();
+        tree.resize(4 * n, LLONG_MAX);
+        lazy.resize(4 * n, 0);
+        lazySet.resize(4 * n, false);
+        build(arr, 0, 0, n - 1);
+    }
+
+    void updateRange(int l, int r, int value)
+    {
+        updateRange(0, 0, n - 1, l, r, value);
+    }
+
+    long long queryRange(int l, int r)
+    {
+        return queryRange(0, 0, n - 1, l, r);
+    }
+};
+int gcd(int a, int b)
+{
+    while (b)
+    {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+// Golden Rules
+/*
+   Solutions are simple.
+
+   Proofs are simple.
+
+   Implementations are simple.
+*/
+void dfs(int node, vvi &adj, vi &toposort, vector<int> &vis)
+{
+    vis[node] = 1;
+    for (auto &it : adj[node])
+        if (!vis[it])
+            dfs(it, adj, toposort, vis);
+    toposort.push_back(node);
+}
+
+void solve()
+{
+    string s;
+    vii edges;
+    map<string, int> mp;
+    while (getline(cin, s))
+    {
+        stringstream ss(s);
+        string word;
+        ss >> word;
+        word.pop_back();
+        if (mp.find(word) == mp.end())
+        {
+            mp[word] = mp.size() + 1;
+        }
+        string temp;
+        while (ss >> temp)
+        {
+            if (mp.find(temp) == mp.end())
+            {
+                mp[temp] = mp.size() + 1;
+            }
+            edges.pb({mp[word], mp[temp]});
+        }
+    }
+    vvi adj(mp.size() + 1);
+    vvi transpose(mp.size() + 1);
+    for (auto &it : edges)
+    {
+        adj[it[0]].pb(it[1]);
+        transpose[it[1]].pb(it[0]);
+    }
+    // toposort
+    vector<int> vis(mp.size() + 1, 0);
+    vi toposort;
+    for (int i = 1; i <= mp.size(); i++)
+    {
+        if (!vis[i])
+            dfs(i, adj, toposort, vis);
+    }
+    // we need to find number of paths from each node to out
+    int special = mp["out"];
+    vi dp(mp.size() + 1, 0);
+    dp[special] = 1;
+    for (auto &it : toposort)
+    {
+        for (auto &child : transpose[it])
+        {
+            dp[child] += dp[it];
+        }
+    }
+    vi dp1(mp.size() + 1, 0);
+    dp1[mp["dac"]] = 1;
+    for (auto &it : toposort)
+    {
+        for (auto &child : transpose[it])
+        {
+            dp1[child] += dp1[it];
+        }
+    }
+    vi dp2(mp.size() + 1, 0);
+    dp2[mp["fft"]] = 1;
+    for (auto &it : toposort)
+    {
+        for (auto &child : transpose[it])
+        {
+            dp2[child] += dp2[it];
+        }
+    }
+    int ans1 = dp[mp["dac"]] * dp1[mp["fft"]] * dp2[mp["svr"]];
+    cout << dp[mp["svr"]] << " " << ans1 << endl;
+}
+
+signed main()
+{
+
+    fast_io;
+    auto start = chrono::high_resolution_clock::now();
+    solve();
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end - start;
+    cout << "Time elapsed: " << elapsed.count() << " seconds" << endl;
+    return 0;
+}
---
+++ 2025/DAY9/main.cpp
@@ -0,0 +1,298 @@
+/**
+ * author:VectorVirtuoso
+ * created: 16:08:27 on 10-12-2025
+ **/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <bitset>
+#include <iterator>
+#include <numeric>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <array>
+#include <functional>
+#include <type_traits>
+#include <exception>
+#include <cassert>
+#include <climits>
+#include <cfloat>
+#include <cctype>
+#include <chrono>
+#include <complex>
+#include <random>
+#include <valarray>
+#include <locale>
+#include <assert.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <limits.h>
+#include <float.h>
+using namespace std;
+#define int ll
+#define endl '\n' // comment for interactive
+#define fast_io              \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define pb push_back
+#define re resize
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define all1(x) (x).begin() + 1, (x).end()
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define loop1(i, n) for (int i = 1; i <= n; i++)
+#define print(x) cout << #x << ": " << x << endl \
+                      << flush
+typedef long long ll;
+typedef vector<int> vi;
+typedef array<int, 2> ii;
+typedef array<int, 3> ti;
+typedef vector<ii> vii;
+typedef vector<ti> vti;
+typedef vector<vi> vvi;
+typedef priority_queue<int> pq;
+template <class T>
+bool ckmin(T &a, T b)
+{
+    bool B = a > b;
+    a = min(a, b);
+    return B;
+}
+template <class T>
+bool ckmax(T &a, T b)
+{
+    bool B = a < b;
+    a = max(a, b);
+    return B;
+}
+const int inf = 1e17;
+const int mod = 1e9 + 7;
+int expo(int a, int b)
+{
+    if (a == 0 && b == 0)
+    {
+        return 1;
+    }
+    if (a == 0)
+    {
+        return 0;
+    }
+    if (b == 1)
+    {
+        return a;
+    }
+    if (b == 0)
+    {
+        return 1;
+    }
+    int x = expo(a, b / 2);
+    if (b % 2 == 0)
+    {
+        return (x) * (x) % mod;
+    }
+    else
+    {
+        return (a * (x) % mod) * (x) % mod;
+    }
+}
+int fac(int a)
+{
+    if (a == 0 || a == 1)
+    {
+        // factorial[a] = 1;
+        return 1;
+    }
+    return (fac(a - 1) * a) % mod;
+}
+int nCr(int n, int r)
+{
+    int mod1 = fac(n);
+    int mod2 = expo(fac(r), mod - 2);
+    int mod3 = expo(fac(n - r), mod - 2);
+    return (((mod1 * mod2) % mod) * mod3) % mod;
+}
+class SegmentTree
+{
+private:
+    vector<long long> tree, lazy;
+    vector<bool> lazySet;
+    int n;
+
+    void build(vector<int> &arr, int node, int start, int end)
+    {
+        if (start == end)
+        {
+            tree[node] = arr[start];
+        }
+        else
+        {
+            int mid = (start + end) / 2;
+            build(arr, 2 * node + 1, start, mid);
+            build(arr, 2 * node + 2, mid + 1, end);
+            tree[node] = min(tree[2 * node + 1], tree[2 * node + 2]);
+        }
+    }
+
+    void propagate(int node, int start, int end)
+    {
+        if (lazySet[node])
+        {
+            tree[node] = lazy[node];
+
+            if (start != end)
+            {
+                lazySet[2 * node + 1] = true;
+                lazySet[2 * node + 2] = true;
+                lazy[2 * node + 1] = lazy[node];
+                lazy[2 * node + 2] = lazy[node];
+            }
+
+            lazy[node] = 0;
+            lazySet[node] = false;
+        }
+    }
+
+    void updateRange(int node, int start, int end, int l, int r, int value)
+    {
+        propagate(node, start, end);
+
+        if (start > r || end < l)
+        {
+            return;
+        }
+
+        if (start >= l && end <= r)
+        {
+            lazySet[node] = true;
+            lazy[node] = value;
+            propagate(node, start, end);
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        updateRange(2 * node + 1, start, mid, l, r, value);
+        updateRange(2 * node + 2, mid + 1, end, l, r, value);
+
+        tree[node] = min(tree[2 * node + 1], tree[2 * node + 2]);
+    }
+
+    long long queryRange(int node, int start, int end, int l, int r)
+    {
+        propagate(node, start, end);
+
+        if (start > r || end < l)
+        {
+            return LLONG_MAX; // Neutral value for min query
+        }
+
+        if (start >= l && end <= r)
+        {
+            return tree[node];
+        }
+
+        int mid = (start + end) / 2;
+        long long leftQuery = queryRange(2 * node + 1, start, mid, l, r);
+        long long rightQuery = queryRange(2 * node + 2, mid + 1, end, l, r);
+
+        return min(leftQuery, rightQuery);
+    }
+
+public:
+    SegmentTree(vector<int> &arr)
+    {
+        n = arr.size();
+        tree.resize(4 * n, LLONG_MAX);
+        lazy.resize(4 * n, 0);
+        lazySet.resize(4 * n, false);
+        build(arr, 0, 0, n - 1);
+    }
+
+    void updateRange(int l, int r, int value)
+    {
+        updateRange(0, 0, n - 1, l, r, value);
+    }
+
+    long long queryRange(int l, int r)
+    {
+        return queryRange(0, 0, n - 1, l, r);
+    }
+};
+int gcd(int a, int b)
+{
+    while (b)
+    {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+// Golden Rules
+/*
+   Solutions are simple.
+
+   Proofs are simple.
+
+   Implementations are simple.
+*/
+
+void solve()
+{
+    vii v;
+    string s;
+    while (getline(cin, s))
+    {
+        int x, y;
+        char c;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == ',')
+            {
+                s[i] = ' ';
+            }
+        }
+        stringstream ss(s);
+        ss >> x >> y;
+        v.pb({x, y});
+    }
+    int n = v.size();
+    int ans = 0;
+    int ans2 = 0;
+    loop(i, n)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            ans = max(ans, (abs(v[i][0] - v[j][0]) + 1) * (abs(v[i][1] - v[j][1]) + 1));
+        }
+    }
+    cout << ans << " ";
+    cout << ans2 << endl;
+}
+
+signed main()
+{
+
+    fast_io;
+    auto start = chrono::high_resolution_clock::now();
+    solve();
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double, milli> duration = end - start;
+    cout << "Time: " << duration.count() << " ms" << endl;
+    return 0;
+}
